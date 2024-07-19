bf7b4b01968ca6920eae5f394bb5c4c2fdcaf0c5
Molina-Q <quentinmolina1@gmail.com>
2024-07-19 16:21:44 +0200

    feat: Add "Add +" button to Home page for creating new articles

--- app/page.tsx
+++ app/page.tsx
@@ -19,8 +19,13 @@ export default function Home() {
   }, []);
 
   return (
-    <main className="flex min-h-screen flex-col  justify-between p-24 bg-slate-950">
+    <main className="flex min-h-screen flex-col justify-between p-24 bg-slate-950">
       <h1 className="text-3xl font-semibold pb-10">Blog</h1>
+      <Link href={"/article/add"} className="w-fit h-fit">
+        <div className="mb-10 px-2 py-1 rounded-xl border border-slate-500 w-fit hover:bg-slate-800">
+          Add +
+        </div>
+      </Link>
       <div className="flex flex-col gap-7">
         {
           articles.map((article) =>
