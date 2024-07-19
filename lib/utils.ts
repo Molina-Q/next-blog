32bb1ca805f25afbf1971d5c3b1c96fed9cb1b1a
Molina-Q <quentinmolina1@gmail.com>
2024-07-19 15:14:08 +0200

    feat: Add utility function for merging CSS classes

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,12 @@
+import clsx, { ClassValue } from "clsx";
 import { format } from "date-fns";
+import { twMerge } from "tailwind-merge";
 
 export function formatDate(date: Date): string {
     return format(new Date(date), "MMMM do, yyyy HH:mm") ?? "Date not available";
 }
+
+export function cn(...inputs: ClassValue[]) {
+    return twMerge(clsx(inputs))
+}
+  
