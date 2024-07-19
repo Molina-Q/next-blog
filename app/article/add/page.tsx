f15db7f76be268803946f5feb6066650446075d9
Molina-Q <quentinmolina1@gmail.com>
2024-07-19 16:51:32 +0200

    feat: Add checkbox functionality for article tags on add page

--- app/article/add/page.tsx
+++ app/article/add/page.tsx
@@ -1,12 +1,13 @@
 "use client"
 
 import axios from 'axios';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { z } from 'zod';
 
 const schema = z.object({
     title: z.string().min(3).max(50),
     content: z.string().min(10),
+    checkTags: z.array(z.string()),
 });
 
 type FormData = z.infer<typeof schema>;
@@ -15,7 +16,12 @@ const AddArticlePage: React.FC = () => {
     const [formData, setFormData] = useState<FormData>({
         title: '',
         content: '',
+        checkTags: [],
     });
+
+    console.log(formData);
+
+    const [tags, setTags] = useState<Tag[]>([]);
 
     const [response, setResponse] = useState<string | null>(null);
 
@@ -27,6 +33,15 @@ const AddArticlePage: React.FC = () => {
             [name]: value,
         });
     };
+
+    const handleCheckChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+
+        setFormData({
+            ...formData,
+            checkTags: formData.checkTags.push(value),
+        });
+    }
 
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
@@ -45,6 +60,14 @@ const AddArticlePage: React.FC = () => {
             setResponse('Invalid form data');
         }
     };
+
+    useEffect(() => {
+        axios.get('/api/tag').then((res) => {
+            setTags(res.data);
+        }).catch((error) => {
+            console.error("TAG", error);
+        });
+    }, []);
 
     return (
         <div className="container mx-auto">
@@ -72,6 +95,19 @@ const AddArticlePage: React.FC = () => {
                         value={formData.content}
                         onChange={handleInputChange}
                     />
+                </div>
+
+                <div>
+                    {
+                        tags.map((tag) => (
+                            <div className='flex flex-row gap-2' key={tag.id} >
+                                <label htmlFor={tag.id} className="block font-bold mb-2">
+                                    {tag.name}
+                                </label>
+                                <input onChange={handleCheckChange} type="checkbox" name="tags" id={tag.id} value={tag.id} />
+                            </div>
+                        ))
+                    }
                 </div>
 
                 <button type="submit" className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
