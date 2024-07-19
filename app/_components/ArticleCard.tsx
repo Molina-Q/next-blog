846eab73d808183322787d455e114e0b4315bc1b
Molina-Q <quentinmolina1@gmail.com>
2024-07-19 16:40:01 +0200

    feat: Improve ArticleCard component with edit link

--- app/_components/ArticleCard.tsx
+++ app/_components/ArticleCard.tsx
@@ -1,17 +1,19 @@
 "use client"
 import Tags from "@/components/Tags";
 import { formatDate } from "@/lib/utils";
-import axios from "axios";
-import { ar } from "date-fns/locale";
-import Image from "next/image";
-import { FC, useEffect, useState } from "react";
+import Link from "next/link";
 
 const ArticleCard: React.FC<{ article: Article }> = ({ article }) => {
 
     return (
         <article className="border border-slate-600 p-4 rounded-md flex flex-col gap-4 hover:bg-slate-800" key={article.id}>
             <div>
-                <h2 className="text-xl font-semibold">{article.title}</h2>
+                <div className="flex justify-between">
+                    <h2 className="text-xl font-semibold">{article.title}</h2>
+                    <Link href={`/article/${article.slug}/edit`}>
+                        <p className="text-sm font-light">edit</p>
+                    </Link>
+                </div>
                 <time className="text-sm font-light">{formatDate(article.createdAt)}</time>
             </div>
 
