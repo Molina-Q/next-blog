24ca91a0ee69432a58c38bbd0d0a5230aa12c9f7
Molina-Q <quentinmolina1@gmail.com>
2024-07-19 11:38:52 +0200

    chore: Add Tags component to ArticleCard for displaying article tags

--- app/_components/ArticleCard.tsx
+++ app/_components/ArticleCard.tsx
@@ -1,4 +1,5 @@
 "use client"
+import Tags from "@/components/Tags";
 import { formatDate } from "@/lib/utils";
 import axios from "axios";
 import { ar } from "date-fns/locale";
@@ -8,7 +9,6 @@ import { FC, useEffect, useState } from "react";
 const ArticleCard: React.FC<{ article: Article }> = ({ article }) => {
 
     return (
-
         <article className="border border-slate-600 p-4 rounded-md flex flex-col gap-4 hover:bg-slate-800" key={article.id}>
             <div>
                 <h2 className="text-xl font-semibold">{article.title}</h2>
@@ -17,17 +17,7 @@ const ArticleCard: React.FC<{ article: Article }> = ({ article }) => {
 
             <div className="line-clamp-2">{article.content}</div>
 
-            <div className="flex flex-row gap-2">
-                {article.tags.map((tag) => (
-                    <span className="text-sm border border-slate-500 bg-slate-700 rounded-md px-3 py-[1px]" key={tag.tag.id}>{tag.tag.name}</span>
-                ))}
-            </div>
-
-            <div className="flex flex-row gap-2">
-                {article.tags.map((tag) => (
-                    <span className="text-sm border border-slate-500 bg-slate-700 rounded-md px-3 py-[1px]" key={tag.tag.id}>{tag.tag.name}</span>
-                ))}
-            </div>
+            <Tags article={article} />
         </article>
     )
 }
