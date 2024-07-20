ba4e4b45ee11e08d736e2e120135afa6faebd0e1
Molina-Q <quentinmolina1@gmail.com>
2024-07-20 22:14:17 +0200

    feat: Add API route for updating article tags

--- app/api/article/[slug]/edit/route.tsx
+++ app/api/article/[slug]/edit/route.tsx
@@ -1,16 +1,21 @@
 
 import { db } from '@/lib/db';
 import { slugify } from '@/lib/utils';
+import { Prisma } from '@prisma/client';
 import { NextRequest, NextResponse } from 'next/server';
 import React from 'react'
 
+interface TagArticleBasic {
+    id: string;
+    articleId: string;
+    tagId: string;
+  };
 
 export async function POST(
     req: NextRequest, { params }: { params: { slug: string } }
 ) {
     try {
-        const { title, content } = await req.json();
-  
+        const { title, content, checkedTags } = await req.json();
 
         const article = await db.article.update({
             where: {
@@ -22,7 +27,40 @@ export async function POST(
             }
         });
 
-        return NextResponse.json(article);
+        const existingTagArticles = await db.tagArticle.findMany({
+            where: {
+                articleId: article.id
+            }
+        });
+
+        // if the tag is only in the formData i need to add it
+        const tagToAdd = checkedTags.filter((checkedTag: string) => {
+            return !existingTagArticles.some((existingTag: TagArticleBasic) => (existingTag.tagId === checkedTag));
+        });
+        
+        // if the tag is only in the database i need to delete it
+        const tagToDelete = existingTagArticles.filter((existingTag: TagArticleBasic) => {
+            return !checkedTags.some((checkedTag: string) => (existingTag.tagId === checkedTag));
+        }); 
+
+        const deletedTags = await Promise.all(tagToDelete.map(async (tagArticle: TagArticleBasic) => (
+            db.tagArticle.delete({
+                where: {
+                    id: tagArticle.id
+                }
+            })
+        )));
+
+        const addedTags = await Promise.all(tagToAdd.map(async (tagId: string) => (
+            db.tagArticle.create({
+                data: {
+                    articleId: article.id,
+                    tagId
+                }
+            })
+        )));
+
+        return NextResponse.json([article, deletedTags, addedTags]);
 
     } catch (error) {
         console.log("error create Movie : ", error);
