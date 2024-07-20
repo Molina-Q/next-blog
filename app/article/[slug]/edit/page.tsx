a9d9bd17ca434d9bd6ae7d83bc90389bf614ec1b
Molina-Q <quentinmolina1@gmail.com>
2024-07-20 22:14:28 +0200

    feat: Add checkbox functionality for article tags on edit page

--- app/article/[slug]/edit/page.tsx
+++ app/article/[slug]/edit/page.tsx
@@ -1,28 +1,47 @@
 "use client"
 
 import axios from 'axios';
+import { title } from 'process';
 import { useEffect, useState } from 'react';
 import { z } from 'zod';
 
 const schema = z.object({
     title: z.string().min(3).max(50),
     content: z.string().min(10),
+    checkedTags: z.array(z.string()),
 });
 
 type FormData = z.infer<typeof schema>;
 
 export default function EditArticlePage({ params }: { params: { slug: string } }) {
+    const [tags, setTags] = useState<Tag[]>([]);
+
+    const [article, setArticle] = useState<Article>();
+
     const [formData, setFormData] = useState<FormData>({
         title: '',
         content: '',
+        checkedTags: [''],
     });
 
     useEffect(() => {
         axios.get(`/api/article/${params.slug}`).then((res) => {
-            setFormData(res.data);
+            setFormData({
+                title: res.data.title,
+                content: res.data.content,
+                checkedTags: res.data.tags.map((tag: { tagId: Tag }) => tag.tagId)
+            });
+            setArticle(res.data);
         }).catch((error) => {
             console.error("ARTICLE", error);
         });
+
+        axios.get('/api/tag').then((res) => {
+            setTags(res.data);
+        }).catch((error) => {
+            console.error("TAG", error);
+        });
+
     }, []);
 
     const [response, setResponse] = useState<string | null>(null);
@@ -35,6 +54,17 @@ export default function EditArticlePage({ params }: { params: { slug: string } }
             [name]: value,
         });
     };
+
+    const handleCheckChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { value, checked } = e.target;
+
+        setFormData(prev => ({
+            ...prev,
+            checkedTags: checked
+                ? [...prev.checkedTags, value] // if true add the value to the array
+                : prev.checkedTags.filter(tag => tag !== value) // if false remove using filter
+        }));
+    }
 
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
@@ -80,6 +110,26 @@ export default function EditArticlePage({ params }: { params: { slug: string } }
                         value={formData.content}
                         onChange={handleInputChange}
                     />
+                </div>
+
+                <div>
+                    {tags && article &&
+                        tags.map((tag) => (
+                            <div className='flex flex-row gap-2' key={tag.id} >
+                                <label htmlFor={tag.id} className="block font-bold mb-2">
+                                    {tag.name}
+                                </label>
+                                <input
+                                    onChange={handleCheckChange}
+                                    type="checkbox"
+                                    name="checkedTags"
+                                    id={tag.id}
+                                    value={tag.id}
+                                    checked={formData!.checkedTags.some(articleTag => articleTag === tag.id)}
+                                />
+                            </div>
+                        ))
+                    }
                 </div>
 
                 <button type="submit" className="bg-slate-500 hover:bg-sky-950 text-white font-bold py-2 px-4 rounded">
