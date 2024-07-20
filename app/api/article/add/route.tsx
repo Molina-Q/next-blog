00b83c5efca08948c0bbbda709f2229164ef82d6
Molina-Q <quentinmolina1@gmail.com>
2024-07-20 18:18:59 +0200

    feat: Add API route for creating new article and associating tags

--- app/api/article/add/route.tsx
+++ app/api/article/add/route.tsx
@@ -9,8 +9,8 @@ export async function POST(
     req: Request
 ) {
     try {
-        const { title, content } = await req.json();
-  
+        const { title, content, checkedTags } = await req.json();
+
 
         const article = await db.article.create({
             data: {
@@ -20,7 +20,21 @@ export async function POST(
             }
         });
 
-        return NextResponse.json(article);
+        const tagArticles: unknown[] = [];
+        {
+            checkedTags.map(async (tag: string) => {
+                const tagArticle = await db.tagArticle.create({
+                    data: {
+                        tagId: tag,
+                        articleId: article.id,
+                    }
+                });
+                tagArticles.push(tagArticle);
+            })
+        }
+
+
+        return NextResponse.json([article, tagArticles]);
 
     } catch (error) {
         console.log("error create Movie : ", error);
