e642060d307059b496e00d111b917e85c40bc205
Molina-Q <quentinmolina1@gmail.com>
2024-07-21 21:40:46 +0200

    feat: Add delete functionality to ArticleCard component

--- app/_components/ArticleCard.tsx
+++ app/_components/ArticleCard.tsx
@@ -1,22 +1,31 @@
 "use client"
 import Tags from "@/components/Tags";
 import { formatDate } from "@/lib/utils";
+import axios from "axios";
 import Link from "next/link";
 
 const ArticleCard: React.FC<{ article: Article }> = ({ article }) => {
+
+    const handleDelete = async () => {
+        axios.delete(`/api/article/${article.slug}/delete`).then((res) => {
+            console.log(res.data);
+        }).catch((error) => {
+            console.log(error);
+        });
+    }
 
     return (
         <article className="border border-slate-600 p-4 rounded-md flex flex-col gap-4 hover:bg-slate-800" key={article.id}>
             <div>
                 <div className="flex justify-between">
                     <h2 className="text-xl font-semibold">{article.title}</h2>
-                    <div className="flex gap-1">
+                    <div className="flex gap-4">
                         <Link href={`/article/${article.slug}/edit`}>
-                            <p className="text-sm font-light text-red-700">X</p>
-                        </Link>
-                        <Link href={`/article/${article.slug}/delete`}>
                             <p className="text-sm font-light">edit</p>
                         </Link>
+                        <p onClick={handleDelete}>
+                            <p className="text-sm font-black text-red-700">X</p>
+                        </p>
                     </div>
                 </div>
                 <time className="text-sm font-light">{formatDate(article.createdAt)}</time>
