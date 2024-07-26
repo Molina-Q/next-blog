7eacc85b4edd163846bb4bcb9cd17fa1829f00b0
Molina-Q <quentinmolina1@gmail.com>
2024-07-26 10:48:39 +0200

    feat: Add delete functionality to article details page

--- app/article/[slug]/page.tsx
+++ app/article/[slug]/page.tsx
@@ -3,9 +3,11 @@
 import Tags from '@/components/Tags';
 import { formatDate } from '@/lib/utils';
 import axios from 'axios'
+import { redirect, useRouter } from 'next/navigation';
 import React, { useEffect } from 'react'
 
 export default function articleDetails({ params }: { params: { slug: string } }) {
+    const router = useRouter();
 
     const [article, setArticle] = React.useState<Article>();
 
@@ -17,13 +19,13 @@ export default function articleDetails({ params }: { params: { slug: string } })
         });
     }, [])
 
-    const handleDelete = async (e: React.ChangeEvent<unknown>) => {
-        axios.delete(`/api/article/${e.target.id}/delete`).then((res) => {
+    const handleDelete = async (e: React.MouseEvent<HTMLParagraphElement>) => {
+        axios.delete(`/api/article/${e.currentTarget.id}/delete`).then((res) => {
             console.log(res.data);
+            router.push('/');
         }).catch((error) => {
             console.log(error);
-        }
-        );
+        });
     }
 
     return (
@@ -33,7 +35,7 @@ export default function articleDetails({ params }: { params: { slug: string } })
                     <div>
                         <h2 className="text-xl font-semibold">{article?.title}</h2>
                         <time className="text-sm font-light">{article?.createdAt && formatDate(article?.createdAt)}</time>
-                        <button onClick={handleDelete} id={article.slug}>Delete</button>
+                        <p onClick={handleDelete} id={article.slug} className='cursor-pointer hover:underline w-fit'>Delete</p>
                     </div>
 
                     <div className="line-clamp-2">{article?.content}</div>
@@ -41,14 +43,15 @@ export default function articleDetails({ params }: { params: { slug: string } })
                     <Tags article={article!} />
 
                     <section className='divide-y-[1px]'>
-                        {article.comments.length === 0
-                            ? <h3 className='text-lg font-semibold'>No comments yet</h3>
-                            : article?.comments.map((comment) => (
-                                <div key={comment.id} className="p-4 flex justify-between">
-                                    <p>{comment.content}</p>
-                                    <time className='text-sm'>{formatDate(comment.createdAt)}</time>
-                                </div>
-                            ))
+                        {
+                            article.comments.length === 0
+                                ? <h3 className='text-lg font-semibold'>No comments yet</h3>
+                                : article.comments.map((comment) => (
+                                    <div key={comment.id} className="p-4 flex justify-between">
+                                        <p>{comment.content}</p>
+                                        <time className='text-sm'>{formatDate(comment.createdAt)}</time>
+                                    </div>
+                                ))
 
                         }
                     </section>
