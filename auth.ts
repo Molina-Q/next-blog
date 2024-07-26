671bded34776ebe836ca3bf6cf4e97d5a700f5b1
Molina-Q <quentinmolina1@gmail.com>
2024-07-26 10:48:15 +0200

    feat: Add authentication functionality using NextAuth

---
+++ auth.ts
@@ -0,0 +1,49 @@
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+
+import { saltAndHashPassword } from "./lib/password";
+import { getUserFromDb } from "./lib/getUser";
+import { signInSchema } from "./lib/zod";
+import { ZodError } from "zod";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+    providers: [
+        Credentials({
+            // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+            // e.g. domain, username, password, 2FA token, etc.
+            credentials: {
+                email: {},
+                password: {},
+            },
+            authorize: async (credentials) => {
+                try {
+                    let user = null;
+
+                    const { email, password } = await signInSchema.parseAsync(credentials);
+
+                    // logic to salt and hash password
+                    const pwHash = saltAndHashPassword(password);
+
+                    // logic to verify if the user exists
+                    user = await getUserFromDb(email, pwHash);
+
+                    if (!user) {
+                        // No user found, so this is their first attempt to login
+                        // meaning this is also the place you could do registration
+                        throw new Error("User not found.");
+                    }
+                    // return user object with their profile data
+                    return user;
+                } catch (error) {
+                    if (error instanceof ZodError) {
+                        // Return `null` to indicate that the credentials are invalid
+                        console.log("ZodError", error.errors);
+                        return null;
+                    }
+                    console.log("Error", error);
+                    return null;
+                }
+            },
+        }),
+    ],
+});
