5188b207cb7d7ff8b899cf78b81600884cf56c67
Molina-Q <quentinmolina1@gmail.com>
2024-07-28 17:55:11 +0200

    refactor: Remove commented out code in signUp/route.tsx

--- app/api/signUp/route.tsx
+++ app/api/signUp/route.tsx
@@ -4,7 +4,7 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(
     req: NextRequest
-) {    
+) {
     try {
         const { email, password } = await req.json();
 
@@ -31,39 +31,3 @@ export async function POST(
         return new NextResponse("Internal Server Error", { status: 500 });
     }
 }
-
-// import { db } from '@/lib/db'
-// import type { NextApiRequest, NextApiResponse } from 'next'
-
-// export default async function handler(
-//     req: NextApiRequest,
-//     res: NextApiResponse
-// ) {
-//     if (req.method !== 'POST') {
-//         return res.status(400).json('Method not allowed')
-//     }
-
-//     try {
-//         const { email, password } = req.body
-
-//         // Validate the input
-//         if (!email || !password) {
-//             return res.status(400).json({ message: 'Email and password are required' })
-//         }
-
-//         // Create the user
-//         const user = await db.user.create({
-//             data: {
-//                 email,
-//                 password,
-//             },
-//         })
-
-
-//         // Return the created user (excluding sensitive information like the password)
-//         res.status(201).json({ user: { id: user.id, email: user.email } })
-//     } catch (error) {
-//         console.error('Error in signup:', error)
-//         res.status(500).json({ message: 'Error creating user' })
-//     }
-// }
