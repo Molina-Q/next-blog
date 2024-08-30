8303491c29cfd42262cecfc1691db57368488699
Molina-Q <quentinmolina1@gmail.com>
2024-08-30 17:28:25 +0200

    refactor: Update authentication logic in auth.ts

--- auth.ts
+++ auth.ts
@@ -1,54 +1,46 @@
-import NextAuth from "next-auth";
-import Credentials from "next-auth/providers/credentials";
+import NextAuth from "next-auth"
+import { ZodError } from "zod"
+import Credentials from "next-auth/providers/credentials"
+import { signInSchema } from "./lib/zod"
+import { saltAndHashPassword } from "./lib/password"
+import { getUserFromDb } from "./lib/getUser"
+// Your own logic for dealing with plaintext password strings; be careful!
 
-import { saltAndHashPassword } from "./lib/password";
-import { getUserFromDb } from "./lib/getUser";
-import { signInSchema } from "./lib/zod";
-import { ZodError } from "zod";
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import { PrismaClient } from "@prisma/client"
- 
-const prisma = new PrismaClient()
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
-    adapter: PrismaAdapter(prisma),
+export const { handlers, auth } = NextAuth({
     providers: [
         Credentials({
             // You can specify which fields should be submitted, by adding keys to the `credentials` object.
             // e.g. domain, username, password, 2FA token, etc.
             credentials: {
-                email: {  label: "email", type: "email" },
-                password: { label: "password", type: "password" },
+                email: {},
+                password: {},
             },
             authorize: async (credentials) => {
                 try {
-                    let user = null;
+                    let user = null
 
-                    const { email, password } = await signInSchema.parseAsync(credentials);
+                    const { email, password } = signInSchema.parse(credentials)
 
                     // logic to salt and hash password
-                    const pwHash = saltAndHashPassword(password);
+                    const pwHash = saltAndHashPassword(password)
 
                     // logic to verify if the user exists
-                    user = await getUserFromDb(email, pwHash);
+                    user = await getUserFromDb(email, pwHash)
 
-                    if (!user) {
-                        // No user found, so this is their first attempt to login
-                        // meaning this is also the place you could do registration
-                        throw new Error("User not found.");
+                    if (typeof(user) === "undefined") {
+                        throw new Error("User not found.")
                     }
-                    // return user object with their profile data
-                    return user;
+
+                    // return JSON object with the user data
+                    return user
                 } catch (error) {
                     if (error instanceof ZodError) {
                         // Return `null` to indicate that the credentials are invalid
-                        console.log("ZodError", error.errors);
-                        return null;
+                        return null
                     }
-                    console.log("Error", error);
-                    return null;
                 }
             },
         }),
     ],
-});
+})
